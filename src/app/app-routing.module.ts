cc6225d49b06d29252d91f83dc2d51c7b7c93ab9
Nikunj_italiya <nikunj.italiya@ghanshyamdigital.com>
2023-02-27 18:41:49 +0530

    monday created project setup

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {HomeComponent} from "./component/home/home.component";
+import {RegistrationComponent} from "./component/registration/registration.component";
+import {LoginComponent} from "./component/login/login.component";
+import {PagenotfoundComponent} from "./component/pagenotfound/pagenotfound.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'', component:HomeComponent },
+  {path:'registration', component:RegistrationComponent },
+  {path:'login', component:LoginComponent },
+  { path: '**', pathMatch: 'full',
+    component: PagenotfoundComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './component/login/login.component';
+import { RegistrationComponent } from './component/registration/registration.component';
+import { UserDetailsComponent } from './component/user-details/user-details.component';
+import { HomeComponent } from './component/home/home.component';
+import { CustomerComponent } from './component/customer/customer.component';
+import { PagenotfoundComponent } from './component/pagenotfound/pagenotfound.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegistrationComponent,
+    UserDetailsComponent,
+    HomeComponent,
+    CustomerComponent,
+    PagenotfoundComponent
   ],
   imports: [
     BrowserModule,
